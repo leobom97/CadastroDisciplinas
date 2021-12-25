907e8a8687d3910d3eab9b40538f488d2d986ade
Leonardo <leordoria2016@gmail.com>
2021-12-25 18:01:04 -0300

    Primeiro Commit da Aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Disciplina } from 'disciplina.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'school-app';
+  editando:any = null
+  nome:any = null;
+  descricao:any = null
+  disciplinas =[
+    new Disciplina('Língua Portuguesa', 'O objetivo norteador bla blá'),
+    new Disciplina('Educação Física', 'A educação física é blá blá blá'),
+    new Disciplina('Ciências','Ciências é a melhor matéria do ensino fundamental')
+  ];
+
+  
+  salvar(){
+    if (this.editando) {
+      this.editando.nome = this.nome;
+      this.editando.descricao = this.descricao;
+    } else {
+      const d = new Disciplina(this.nome, this.descricao);
+      this.disciplinas.push(d);
+    }
+    this.nome = null;
+    this.descricao = null;
+    this.editando = null;
+  }
+  
+  excluir(disciplina:any) {
+   if (this.editando == disciplina){
+     alert('Você não pode excluir uma disciplina que stá editando');
+   }else{
+     if (confirm('Tem certeza que deseja excluir a disciplina"'
+        + disciplina.nome + '"?')){
+          const i = this.disciplinas.indexOf(disciplina);
+          this.disciplinas.splice(i, 1)
+        }
+   }
+  }
+
+  editar(disciplina:any){
+    this.nome = disciplina.nome;
+    this.descricao = disciplina.descricao
+    this.editando = disciplina
+  }
+
+  cancelar(){
+    this.nome = null
+    this.descricao = null
+    this.editando = null
+  }
+
 }
